9474c00f18d6ce558781186b4df9c767d17abfbd
Namira <nekohalal@gmail.com>
2025-05-21 17:21:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image(23472b9e36efc7644af62121cf435fb2 (1).jpg, width=200)
